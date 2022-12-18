e9144e2b26981706dd24eaa56b6d7d2dac466cda
Serkan Sisman <srknssmn@gmail.com>
2022-12-18 17:04:07 +0300

    update

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function () {
-  console.log('Function Working');
-};
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Phote Name',
+    description: 'Photo description',
+  };
+  res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda baslatildi..`);
+});
