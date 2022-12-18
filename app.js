c2b8ddd6b2fc681f6947bc39dc36c51490e5ffae
Serkan Sisman <srknssmn@gmail.com>
2022-12-18 22:17:22 +0300

    public klasörü ve ejs modülü eklendi

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express'); // express modülü
+const ejs = require('ejs');
 const path = require('path'); // PATH modülü
+
 const app = express();
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 /*
 // Middleware Örnekleri
@@ -17,13 +22,14 @@ const myLogger2 = (req, res, next) => {
 
 // MIDDLEWARES
 app.use(express.static('public')); // Static Files Middleware
+
 // Middleware örneklerini çalıştırmak.
 // app.use(myLogger); // Kendi oluşturduğumuz Middleware 1
 // app.use(myLogger2); // Kendi oluşturduğumuz Middleware 2
 
+// ROUTES
+// app.get de bir Middlewaredir.
 app.get('/', (req, res) => {
-  // Bu da bir middlewaredir.
-
   // Örnek "send" Gönderimi.
   /*const photo = {
     id: 1,
@@ -32,8 +38,17 @@ app.get('/', (req, res) => {
   };
   res.send(photo);
   */
+  // ejs olmadan çalıştırmak için bu şekilde.
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html')); // Path __mkdir ile klasör bulunuyor.
+  res.render('index');
+});
 
-  res.sendFile(path.resolve(__dirname, 'temp/index.html')); // Path __mkdir ile klasör bulunuyor.
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
