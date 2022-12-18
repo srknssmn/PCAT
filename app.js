5dfddbc4d88639a52c40a073928296b2cee5a41c
Serkan Sisman <srknssmn@gmail.com>
2022-12-18 18:31:55 +0300

    update

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-const express = require('express');
+const express = require('express'); // express modülü
+const path = require('path')  // PATH modülü
 const app = express();
 
+/*
+// Middleware Örnekleri
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next(); // Middleware bir sonrakine aktarıldı.
+};
+
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next(); // Middleware bir sonrakine aktarıldı.
+};
+*/
+
+// MIDDLEWARES
+app.use(express.static('public')); // Static Files Middleware
+// Middleware örneklerini çalıştırmak.
+// app.use(myLogger); // Kendi oluşturduğumuz Middleware 1
+// app.use(myLogger2); // Kendi oluşturduğumuz Middleware 2
+
 app.get('/', (req, res) => {
-  const photo = {
+  // Bu da bir middlewaredir.
+
+  // Örnek "send" Gönderimi.
+  /*const photo = {
     id: 1,
     name: 'Phote Name',
     description: 'Photo description',
   };
   res.send(photo);
+  */
+
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));   // Path __mkdir ile klasör bulunuyor.
 });
 
 const port = 3000;
