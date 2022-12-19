90dd19527ea7b5669c3bb7b29e47d622dd694a5f
Serkan Sisman <srknssmn@gmail.com>
2022-12-20 01:22:10 +0300

    update

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express'); // express modülü
-const ejs = require('ejs');
+const ejs = require('ejs'); // EJS modülü
 const path = require('path'); // PATH modülü
 
 const app = express();
 
 // TEMPLATE ENGINE
+// EJS - Dinamik modülünü aktifleştirdir.
 app.set('view engine', 'ejs');
 
+// Statik modülü aktifleştirildi.
+app.use(express.static('public')); // Static Files Middleware
+
+// MIDDLEWARES
 /*
 // Middleware Örnekleri
 const myLogger = (req, res, next) => {
@@ -18,18 +23,15 @@ const myLogger2 = (req, res, next) => {
   console.log('Middleware Log 1');
   next(); // Middleware bir sonrakine aktarıldı.
 };
-*/
-
-// MIDDLEWARES
-app.use(express.static('public')); // Static Files Middleware
-
 // Middleware örneklerini çalıştırmak.
-// app.use(myLogger); // Kendi oluşturduğumuz Middleware 1
-// app.use(myLogger2); // Kendi oluşturduğumuz Middleware 2
+app.use(myLogger); // Kendi oluşturduğumuz Middleware 1
+app.use(myLogger2); // Kendi oluşturduğumuz Middleware 2
+*/
 
 // ROUTES
 // app.get de bir Middlewaredir.
 app.get('/', (req, res) => {
+  res.render('index');
   // Örnek "send" Gönderimi.
   /*const photo = {
     id: 1,
@@ -40,7 +42,6 @@ app.get('/', (req, res) => {
   */
   // ejs olmadan çalıştırmak için bu şekilde.
   // res.sendFile(path.resolve(__dirname, 'temp/index.html')); // Path __mkdir ile klasör bulunuyor.
-  res.render('index');
 });
 
 app.get('/about', (req, res) => {
