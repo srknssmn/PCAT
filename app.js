748a9c38d304d1f44af2620540d56bc4ff1d486d
Serkan Sisman <srknssmn@gmail.com>
2022-12-25 22:13:42 +0300

    mongodb atlas cloud

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ const app = express();
 
 //connect DB
 // Kendi sunucumuzda database oluşturuldu.
-mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
+// mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
+mongoose.connect('mongodb+srv://srknssmn:seko2405@cluster0.kj73wkn.mongodb.net/pcat-db?retryWrites=true&w=majority').then(() => {
+  console.log('DB CONNECTED')
+}).catch((err) => {
+  console.log(err)
+})
+
 
 // *** TEMPLATE ENGINE ***
 // EJS - Dinamik modülünü aktifleştirdir.
@@ -171,7 +177,8 @@ app.delete('/photos/:id', async (req, res) => {
   res.redirect('/'); // Ana sayfaya yönlendirme.
 });
 
-const port = 3000;
+// const port =3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda baslatildi..`);
 });
